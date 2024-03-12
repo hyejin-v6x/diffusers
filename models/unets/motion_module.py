b256ce59acf3dfea0026a811ed38239f4efebc21
hyejin-v6x <hyejin.shin@voyagerx.com>
2024-03-12 15:40:45 +0900

    change positional_encoding_mas_len

--- models/unets/motion_module.py
+++ models/unets/motion_module.py
@@ -86,7 +86,7 @@ class VanillaTemporalModule(nn.Module):
         attention_block_types              =( "Temporal_Self", "Temporal_Self" ),
         cross_frame_attention_mode         = None,
         temporal_position_encoding         = False,
-        temporal_position_encoding_max_len = 24,
+        temporal_position_encoding_max_len = 32,
         temporal_attention_dim_div         = 1,
         zero_initialize                    = True,
     ):
@@ -142,7 +142,7 @@ class TemporalTransformer3DModel(nn.Module):
         
         cross_frame_attention_mode         = None,
         temporal_position_encoding         = False,
-        temporal_position_encoding_max_len = 24,
+        temporal_position_encoding_max_len = 32,
     ):
         super().__init__()
 
@@ -222,7 +222,7 @@ class TemporalTransformerBlock(nn.Module):
         upcast_attention                   = False,
         cross_frame_attention_mode         = None,
         temporal_position_encoding         = False,
-        temporal_position_encoding_max_len = 24,
+        temporal_position_encoding_max_len = 32,
     ):
         super().__init__()
 
@@ -275,7 +275,7 @@ class PositionalEncoding(nn.Module):
         self, 
         d_model, 
         dropout = 0., 
-        max_len = 24
+        max_len = 32
     ):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
@@ -297,7 +297,7 @@ class VersatileAttention(CrossAttention):
             attention_mode                     = None,
             cross_frame_attention_mode         = None,
             temporal_position_encoding         = False,
-            temporal_position_encoding_max_len = 24,            
+            temporal_position_encoding_max_len = 32,            
             *args, **kwargs
         ):
         super().__init__(*args, **kwargs)
